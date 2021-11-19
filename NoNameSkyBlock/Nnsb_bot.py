b7ed5eca9fb71fd8ddf3d62e714f48db64c215c5
NoNameSkyBlocks <71970893+kirigami-0@users.noreply.github.com>
2021-11-19 10:44:50 +0900

    Update Nnsb_bot.py

--- NoNameSkyBlock/Nnsb_bot.py
+++ NoNameSkyBlock/Nnsb_bot.py
@@ -1,17 +1,17 @@
+#いろんなものをインポート
 import discord
 from discord import activity
 from discord.colour import Color
 from discord.ext import commands
-from discord.flags import Intents
-intents = discord.Intents.all()
 
+#ボットの基本設定
 bot = commands.Bot(
     command_prefix="n.",
     help_command=None,
     activity=discord.Game("NNSB"),
-    intents=intents
     )
 
+#コグを有効化
 bot.load_extension('Cog.Omikuji')
 bot.load_extension('Cog.Reaction_roll')
 bot.load_extension('Cog.Ng_word')
@@ -20,4 +20,5 @@ bot.load_extension('Cog.Help')
 bot.load_extension('Cog.Url')
 bot.load_extension('Cog.Join')
 
+#ボット起動
 bot.run('TOKEN')
